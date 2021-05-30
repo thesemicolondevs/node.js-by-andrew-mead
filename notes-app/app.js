43331dc4e444b9cf3fa54f01b19fe6f1a94ca808
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2021-05-30 19:09:39 +0530

    :art: improved the format of the code

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,3 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.writeFileSync('notes.txt', "This file is created by Node.js!");
+fs.writeFileSync("notes.txt", "This file is created by Node.js!");
