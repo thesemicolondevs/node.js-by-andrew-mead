66c857eb4394d2b040adb162be591626d07d10e1
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2021-08-07 22:38:09 +0530

    learned how to ppend data to a file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,14 @@
 const fs = require("fs");
 
-fs.writeFileSync("notes.txt", "This file is created by Node.js!");
+fs.writeFileSync("notes.txt", "My Name is Aditya.");
+
+/*
+  @[Challange 1]: Append a message to notes.txt
+
+  @[Steps]
+  1) use appendFileSync to append in the file
+  2) Run the script
+  3) Check your work by opening the file & viewing the appended text
+*/
+
+fs.appendFileSync("notes.txt", " I live in India.");
