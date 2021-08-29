298976b3b9d03886a5f2805203fd37a1a05e18e6
adityatheoctocatdev <adpatel162@outlook.com>
2021-08-29 12:56:33 +0530

    formatting the files using prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
     "no-restricted-syntax": [
       "error",
       {
-        selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        selector:
+          "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
         message: "Unexpected property on console object was called",
       },
     ],
