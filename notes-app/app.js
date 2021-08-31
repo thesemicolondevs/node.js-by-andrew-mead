5c0e0f7afc34363cd82707af0b0038831c434aa6
adityatheoctocatdev <adpatel162@outlook.com>
2021-08-31 15:33:19 +0530

    created some custom commands using yargs (#54)

--- notes-app/app.js
+++ notes-app/app.js
@@ -22,3 +22,62 @@ const greenMsg = chalk.green.inverse.bold("Success!");
 console.log(greenMsg);
 
 console.log(yargs.argv);
+
+/**
+ * @custiomize yargs version
+ */
+yargs.version("1.1.0");
+
+/**
+ * @create add command
+ */
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: () => {
+    console.log("Adding a new note!");
+  },
+});
+
+/**
+ * @create remove command
+ */
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: () => {
+    console.log("Removing a note!");
+  },
+});
+
+/**
+ * @author: @adityatheoctocatdev
+ *
+ * @challange
+ * @description: create two commands, one to list notes, one to read a note
+ * 1. create a command to list notes
+ * 2. create a command to read a note
+ * 3. test your work
+ */
+
+/**
+ * @create list command
+ */
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: () => {
+    console.log("List out all your notes!");
+  },
+});
+
+/**
+ * @create read command
+ */
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: () => {
+    console.log("Reading a note!");
+  },
+});
